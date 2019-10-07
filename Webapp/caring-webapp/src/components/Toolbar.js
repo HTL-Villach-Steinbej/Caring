67ea59e27e0e67c72a9b8fa433271e5545c01ab8
HTL-Villach-Steinbej <jonathan.steinberger@edu.htl-villach.at>
2019-10-07 11:35:45 +0200

    Some updates

--- Webapp/caring-webapp/src/components/Toolbar.js
+++ Webapp/caring-webapp/src/components/Toolbar.js
@@ -3,20 +3,27 @@ import React from "react";
 function Toolbar(){
     return(
         <div className="toolbar">
-            <div className="toolbar-left-item">
+            <div className="toolbar-left-item" onClick="">
                 <p className="toolbar-item-text">Home</p>
             </div>
-            <div className="toolbar-left-item">
+            <div className="toolbar-left-item" onClick="">
                 <p className="toolbar-item-text">Book</p>
             </div>
-            <div className="toolbar-left-item">
+            <div className="toolbar-left-item" onClick="">
                 <p className="toolbar-item-text">Info</p>
             </div>
-            <div className="toolbar-left-item">
+            <div className="toolbar-left-item" onClick="">
                 <p className="toolbar-item-text">Problems</p>
             </div>
-            <div className="toolbar-left-item">
+            <div className="toolbar-left-item" onClick="">
                 <p className="toolbar-item-text">About</p>
+            </div>
+            <div className="toolbar-right-item">
+                <img
+                    src="./icons/notification_icon.svg"
+                    id="notification-logo"
+                    alt="Notifications Icon"
+                />
             </div>
         </div>
     )
