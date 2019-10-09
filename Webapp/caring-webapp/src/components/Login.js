99bc7ad1af933b1347ccf8c10323c1286b4ed7b6
HTL-Villach-Steinbej <jonathan.steinberger@edu.htl-villach.at>
2019-10-09 09:00:18 +0200

    Bugfix

--- Webapp/caring-webapp/src/components/Login.js
+++ Webapp/caring-webapp/src/components/Login.js
@@ -16,7 +16,7 @@ class Login extends React.Component {
             <div class="login-page">
                 <div class="form">
                     <form class="login-form">
-                        <input type="text" placeholder="Username" onChange={this.setState(state.username, this.value)}/>
+                        <input type="text" placeholder="Username" />
                         <input type="password" placeholder="Password"/>
                         <button onClick={this.login(this.state.username, this.state.password)}>Login</button>
                         <p class="message">Not registered? <a href="#">Create an account</a></p>
