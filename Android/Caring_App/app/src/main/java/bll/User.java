e06d0d277e021f603eccc95e0139ef8551013a48
StichBiceps <do.stich@gmx.at>
2020-04-06 17:56:54 +0200

    Mertsche

---
+++ Android/Caring_App/app/src/main/java/bll/User.java
@@ -0,0 +1,19 @@
+package bll;
+
+public class User {
+
+    private String id;
+
+    public User(String id) {
+        super();
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
